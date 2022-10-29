4cf1c190437bd626e7315ad58c93e94fbc567134
Md Aminul Haque <113428629+programmeraminul@users.noreply.github.com>
2022-10-29 23:41:38 +0600

    message

--- crud_django/settings.py
+++ crud_django/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e&ytxc37ki(erokxuh1mo_$9f3s-3%)n58o@j=xbpkh#*9fh55'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['own-online-course.herokuapp.com','127.0.0.1']
 
@@ -145,9 +145,8 @@ STATIC_URL = '/static/'
 #MEDIA_ROOT = os.path.join(BASE_DIR,'crud/static/crud/files/img')
 #MEDIA_RUL = '/seen/'
 
-#STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
