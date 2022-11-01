413aa55cf8b0deafd17e463f82a32bf975177324
Md Aminul Haque <113428629+programmeraminul@users.noreply.github.com>
2022-11-01 19:26:43 +0600

    message

--- crud/functions.py
+++ crud/functions.py
@@ -4,7 +4,7 @@ import os,uuid
 
 def handle_single_image(f):
     st = str(uuid.uuid4())
-    with open('https://own-online-course.herokuapp.com/staticfiles/mycrud/static/crud/files/img/' + "_" + st + ".jpg", 'wb+') as destination:
+    with open('crud/static/crud/files/img/' + "_" + st + ".jpg", 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
             viewPath = '/static/crud/files/img/' + "_" + st + ".jpg"
--- crud_django/settings.py
+++ crud_django/settings.py
@@ -47,9 +47,6 @@ INSTALLED_APPS = [
     'bootstrap_datepicker_plus',
     "bootstrap4",
     "whitenoise.runserver_nostatic",
-    #'cloudinary_storage',
-    #'cloudinary',
-
 ]
 
 
@@ -147,16 +144,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/ 'static'
 
-#MEDIA_ROOT = os.path.join(BASE_DIR,'crud/static/crud/files/img')
-#MEDIA_RUL = '/seen/'
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfiles/mycrud/static/')]
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
